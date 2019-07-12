a0a256b1bbffa5ad4ade647233f5572924d95c93
耿源 <2788816975@qq.com>
2019-07-12 16:26:42 +0800

    实现login静态页,

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import ElenemtUI from 'element-ui'
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import router from '@/router'
+import router from '@/routers'
 
-Vue.use(ElenemtUI)
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
